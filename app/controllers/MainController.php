22fa09e390b77a984a6aef720503733c2ab1113d
Vahe Hayrapetyan <vahikoh@gmail.com>
2020-02-14 20:32:29 -0500

    Adding three brands from database to index.php

--- app/controllers/MainController.php
+++ app/controllers/MainController.php
@@ -8,7 +8,8 @@ use ishop\Cache;
 class MainController extends AppController
 {
     public function indexAction(){
-        //debug($this->route);
+        $brands = \RedBeanPHP\R::find('brand', 'LIMIT 3');
         $this->setMeta('Main page', 'Description', 'Keywords');
+        $this->set(compact('brands'));
     }
 }
--- config/config_db.php
+++ config/config_db.php
@@ -1,7 +1,7 @@
 <?php
 // this file keeps the data connection configs to db
 return [
-    'dsn' => 'mysql:host=localhost;dbname=myshop1;charset=utf8',
+    'dsn' => 'mysql:host=localhost;dbname=vaheshop;charset=utf8',
     'user'=>'root',
     'pass'=>''
 ];
